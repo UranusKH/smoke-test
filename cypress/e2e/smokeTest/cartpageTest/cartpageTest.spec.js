3275c2969248c92f0c33dba7222a7b05c9292182
UranusKH <Uranuskh92@gmail.com>
2023-07-31 23:24:59 -0700

    test

--- cypress/e2e/smokeTest/cartpageTest/cartpageTest.spec.js
+++ cypress/e2e/smokeTest/cartpageTest/cartpageTest.spec.js
@@ -1,12 +1,18 @@
 /// <reference types="Cypress" />
+import "cypress-xpath";
+const { type } = require("jquery");
 
 describe("Verify 'Cart' page functionalities", function () {
+  let priceTotal = [];
+
   beforeEach(() => {
     cy.viewport(1280, 720);
   });
+
   it("Verify user able to add product in the cart", function () {
     // 1. Go to Tamice.com
-    cy.visit(Cypress.env("url"));
+    // cy.visit(Cypress.env("url"));
+    cy.visit("http://localhost:3000/ticket-site#/");
     cy.contains("Package Tour").click().invoke("show");
     // 2. Hove over 'Package Tour' from the navigation bar
     cy.get("li").contains("NY Big Apple Pass").click({ force: true });
@@ -24,13 +30,14 @@ describe("Verify 'Cart' page functionalities", function () {
     cy.contains("Cart").click();
     cy.url().should("include", "/cart");
     // 6. Verify product is in the 'Cart' page
-    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[1]/div[1]/div[1]").should("contain", "Guide Tour");
-    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[1]/div[1]/div[1]").should("contain", "NY Reserve");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/main[1]/div[1]/div[1]/div[1]").should("contain", "Guide Tour");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/main[1]/div[1]/div[1]/div[1]").should("contain", "NY Reserve");
   });
 
   it("User able to subtract product quantity from the '티켓구입' field", function () {
     // 1. Go to Tamice.com
-    cy.visit(Cypress.env("url"));
+    // cy.visit(Cypress.env("url"));
+    cy.visit("http://localhost:3000/ticket-site#/");
     cy.contains("Package Tour").click().invoke("show");
     // 2. Hove over 'Package Tour' from the navigation bar
     cy.get("li").contains("NY Big Apple Pass").click({ force: true });
@@ -47,7 +54,8 @@ describe("Verify 'Cart' page functionalities", function () {
 
   it("User able to add product quantity from the '티켓구입' field", function () {
     // 1. Go to Tamice.com
-    cy.visit(Cypress.env("url"));
+    // cy.visit(Cypress.env("url"));
+    cy.visit("http://localhost:3000/ticket-site#/");
     cy.contains("Package Tour").click().invoke("show");
     // 2. Hove over 'Package Tour' from the navigation bar
     cy.get("li").contains("NY Big Apple Pass").click({ force: true });
@@ -62,44 +70,97 @@ describe("Verify 'Cart' page functionalities", function () {
   });
 
   it("Verify user able to see product detail after clicking tour list", function () {
-   // 1. Go to Tamice.com
-   cy.visit(Cypress.env("url"));
-   cy.contains("Package Tour").click().invoke("show");
-   // 2. Hove over 'Package Tour' from the navigation bar
-   cy.get("li").contains("NY Big Apple Pass").click({ force: true });
-   // 3. Click 'NY Big Apple Pass'
-   cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]").click().invoke("show");
-   cy.get("li").contains("Big 2").click({ force: true });
-   // 4. Add products
-   cy.get('img.cursor-pointer[src*="increment.svg"]').click();
-   cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").should("contain", "2");
-   cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("Amy June 19 Package Tour").click();
-   cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("NY Reserve").click();
-   cy.contains("Add to the cart").click();
-   // 5. Go to 'Cart' page
-   cy.contains("Cart").click();
-   cy.url().should("include", "/cart");
-   // 6. Verify product is in the 'Cart' page
-
-    
-   cy.contains("Amy June 19 Package Tour").should("exist")
-   cy.contains("NY Reserve").should("exist")
-   cy.contains("Amy June 19 Package Tour").click()
-   cy.contains("NY Reserve").click()
+    // 1. Go to Tamice.com
+    // cy.visit(Cypress.env("url"));
+    cy.visit("http://localhost:3000/ticket-site#/");
+    cy.contains("Package Tour").click().invoke("show");
+    // 2. Hove over 'Package Tour' from the navigation bar
+    cy.get("li").contains("NY Big Apple Pass").click({ force: true });
+    // 3. Click 'NY Big Apple Pass'
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]").click().invoke("show");
+    cy.get("li").contains("Big 2").click({ force: true });
+    // 4. Add products
+    cy.get('img.cursor-pointer[src*="increment.svg"]').click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").should("contain", "2");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("Amy June 19 Package Tour").click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("NY Reserve").click();
+    cy.contains("Add to the cart").click();
+    // 5. Go to 'Cart' page
+    cy.contains("Cart").click();
+    cy.url().should("include", "/cart");
+    // 6. Verify product is in the 'Cart' page
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/main[1]/div[1]/div[1]/div[1]").contains("Amy June 19 Package Tour").should("exist");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/main[1]/div[1]/div[1]/div[1]").contains("NY Reserve").should("exist");
   });
 
   it("Verify user able to continue checkout", function () {
+
+    cy.visit("http://localhost:3000/ticket-site#/");
     // 1. Go to Tamice.com
-    // 2. Hove over 'Package Tour' from the navigation bar
-    // 3. Click 'NY Big Apple Pass'
-    // 4. Add products
-    // 5. Go to 'Cart' page
-    // 6. Click Proceed Checkout
+    cy.contains("Login").click();
+    // 3. Type valid credential
+    cy.get('input[placeholder="Email"]').type("kilhotestaccount@gmail.com");
+    cy.get('input[placeholder="Password"]').type("Kilhotest12#");
+    // 4. Click 'Sign in' button
+    cy.xpath("//button[contains(text(),'Login')]").click();
+    cy.contains("Package Tour").click().invoke("show");
+    cy.get("li").contains("NY Big Apple Pass").click({ force: true });
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]").click().invoke("show");
+    cy.get("li").contains("Big 2").click({ force: true });
+
+    //Add "Big 2" from "Package Tour"
+    cy.contains("Adult").click().invoke("show").should("contain", "Adult");
+    cy.get('img.cursor-pointer[src*="increment.svg"]').click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").should("contain", "2");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("Guide Tour").click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("NY Reserve").click();
+    cy.contains("Add to the cart").click();
+
+    //Add "Big 3" from "Package Tour"
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]").click().invoke("show");
+    cy.get("li").contains("Big 3").click({ force: true });
+    cy.contains("Adult").click().invoke("show");
+    cy.get("li").contains("Child").click({ force: true });
+    cy.get('img.cursor-pointer[src*="increment.svg"]').click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").should("contain", "3");
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("Amy Guid Tour July 20").click();
+    cy.contains("Add to the cart").click();
+    cy.contains("Cart").click({ force: true });
+    cy.url().should("include", "/cart");
+
+    //Check all the Product exist in the "Shopping Cart"
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/main[1]/div[1]/div[1]/div[1]").each(($el) => {
+      // Get the text content of the element and check if it contains 'hello'
+      cy.wrap($el).contains('Guide Tour');
+    });
+
+
+    
   });
 
   it("Verify user able to checkout as guest", function () {
     // 1. Go to Tamice.com
+    cy.visit("http://localhost:3000/ticket-site#/");
     // 2. Hove over 'Package Tour' from the navigation bar
+    cy.contains("Package Tour").click().invoke("show");
+    cy.get("li").contains("NY Big Apple Pass").click({ force: true });
+    // 3. Click 'NY Big Apple Pass'
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]").click().invoke("show");
+    cy.get("li").contains("Big 2").click({ force: true });
+    cy.contains("Adult").click().invoke("show");
+    cy.get('img.cursor-pointer[src*="increment.svg"]').click();
+    cy.get('img.cursor-pointer[src*="increment.svg"]').click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").should("contain", "3");
+
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("Amy June 19 Package Tour").click();
+    cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[6]").contains("NY Reserve").click();
+    cy.contains("Add to the cart").click();
+    // 5. Go to 'Cart' page
+    cy.contains("Cart").click();
+    cy.url().should("include", "/cart");
+    cy.contains("Proceed Checkout").click()
+    cy.contains("Guest Checkout").click()
+    cy.url().should("include", "/cart?guestCheckout=true");
     // 3. Click 'NY Big Apple Pass'
     // 4. Add products
     // 5. Go to Cart Page
@@ -119,5 +180,25 @@ describe("Verify 'Cart' page functionalities", function () {
     // 8. Type valid debit/credit card information
     // 9. Click 'Proceed Checkout' button
     // 10. Verify user able to checkout with valid credit/debit card
+  });
+
+  context("login", () => {
+    //   1. Go to Tamice.com landing page
+    //   2. Click 'Login' button
+    //   3. Click 'Non-Member Ticket Look up' link
+    //   4. Verify 'Email' input field is present on 'Order Lookup' page
+    // });
+    // it(" Verify 'Order Number' input field is present on 'Order Lookup' page   ", function () {
+    //   1. Go to Tamice.com landing page
+    //   2. Click 'Login' button
+    //   3. Click 'Non-Member Ticket Look up' link
+    //   4. Verify 'Order Number' input field is present on 'Order Lookup' page
+    // });
+    // it(" Verify 'Order Look up' button input field is present on 'Order Lookup' page  ", function () {
+    //   1. Go to Tamice.com landing page
+    //   2. Click 'Login' button
+    //   3. Click 'Non-Member Ticket Look up' link
+    //   4. Verify 'Order Look up' button input field is present on 'Order Lookup' page
+    // });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,39 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import 'cypress-plugin-xhr-toggle'
+import "cypress-plugin-xhr-toggle";
 // or
-require('cypress-plugin-xhr-toggle')
+require("cypress-plugin-xhr-toggle");
+
+Cypress.Commands.add("getPrice", (itemIdentifier) => {
+    cy.xpath(itemIdentifier).then(($priceElement) => {
+    const priceTest = $priceElement  
+    const price = parseFloat(priceTest.replace("$", ""));
+    return price;
+  });
+});
+
+
+// cy.getPrice("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[2]").then((price) => {
+//     console.log(price);
+//     cy.xpath("//body/div[@id='root']/div[2]/div[4]/div[1]/div[4]/div[1]/div[1]/div[2]/div[1]/div[3]/div[4]/div[2]").invoke('text').then((text) => {
+//     const quantity = parseInt(text)
+//     console.log(quantity);
+  
+//   }).then(() => {
+
+//   });
+
+//   })
+
+
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
